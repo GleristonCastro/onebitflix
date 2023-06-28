8ea733268de0cb6213df82638dec155edc924820
Gleriston <xpanium@gmail.com>
2023-06-27 22:23:12 -0300

    Adicionado rota dos arquivos estáticos

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,12 @@
 import express from 'express'; //importamos o express
 import { sequelize } from './database';
+import { adminJs, adminJsRouter } from './adminjs';
 
 const app = express(); //instanciamos o express
+
+app.use(express.static('public')); //Informamos ao express onde ficar a rota dos arquivos estáticos.
+
+app.use(adminJs.options.rootPath, adminJsRouter); //Mesma coisa que faziamos com o express, app.use(caminho, rotas)
 
 const PORT = process.env.PORT || 3000; //definimos a porta pelo env.Port ou caso não tenha será 3000
 
@@ -9,6 +14,5 @@ app.listen(PORT, () => { //Iniciamos nossa função de callback
   sequelize.authenticate().then(() => { //chamamos o método authenticate do sequelize
     console.log(`DB Connection sucessfull`) //imprimimos no console caso a conexão com o banco esteja ok
   });
-  console.log(`Server started sucessfuly: http://localhost/:${PORT}`);
+  console.log(`Server started sucessfuly: http://localhost:${PORT}`);
 })
-
