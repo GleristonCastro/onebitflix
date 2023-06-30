504cbd3d6dce55826a56d270a682d94ec56b0eb3
Gleriston <xpanium@gmail.com>
2023-06-30 14:00:54 -0300

    Adicionando a associação Cource

--- src/models/index.ts
+++ src/models/index.ts
@@ -1,6 +1,12 @@
 //Definir as associações dos módulos
-import { Category } from "./Category";
+import { Category } from './Category'
+import { Course } from './Course'
+
+Category.hasMany(Course)
+
+Course.belongsTo(Category)
 
 export {
+  Course,
   Category
 }
