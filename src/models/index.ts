055de10b374bc311fe76846f4d9eb4b682c59d84
Gleriston <xpanium@gmail.com>
2023-06-30 15:05:18 -0300

    Associando Episode com Course

--- src/models/index.ts
+++ src/models/index.ts
@@ -1,12 +1,17 @@
 //Definir as associações dos módulos
 import { Category } from './Category'
 import { Course } from './Course'
+import { Episode } from './Episode'
 
 Category.hasMany(Course)
 
 Course.belongsTo(Category)
+Course.hasMany(Episode)
+
+Episode.belongsTo(Course)
 
 export {
+  Category,
   Course,
-  Category
+  Episode
 }
