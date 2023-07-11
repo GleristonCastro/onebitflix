fddb9f31639cb78c8b3ebe5baace52a2c7638dfb
Gleriston <xpanium@gmail.com>
2023-07-11 19:59:40 -0300

    Adicionando rota para apagar likes

--- src/routes.ts
+++ src/routes.ts
@@ -26,6 +26,7 @@ router.get('/favorites', ensureAuth, favoritesController.index);
 router.post('/favorites', ensureAuth, favoritesController.save);
 router.delete('/favorites', ensureAuth, favoritesController.delete);
 
-router.post('/likes', ensureAuth, likesController.save)
+router.post('/likes', ensureAuth, likesController.save);
+router.delete('/likes', ensureAuth, likesController.delete);
 
 export { router };
