2c06e7d8a23185125fbb6bada4e3f474340b3c1e
Gleriston <xpanium@gmail.com>
2023-07-11 18:33:02 -0300

    Adicição das rotas para listar e excluir favoritos

--- src/routes.ts
+++ src/routes.ts
@@ -21,6 +21,8 @@ router.get('/courses/:id', ensureAuth , coursesController.show);
 
 router.get('/episodes/stream', ensureAuthViaQuery , episodesController.stream);
 
+router.get('/favorites', ensureAuth, favoritesController.index);
 router.post('/favorites', ensureAuth, favoritesController.save);
+router.delete('/favorites', ensureAuth, favoritesController.delete);
 
 export { router };
