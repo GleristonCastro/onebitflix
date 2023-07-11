5bc9366d2f8e355510ea12cc89b2ecdf939375c2
Gleriston <xpanium@gmail.com>
2023-07-11 19:32:15 -0300

    Adicionando associações do like

--- src/models/index.ts
+++ src/models/index.ts
@@ -1,16 +1,16 @@
-// src/models/index.ts
-
 import { Category } from './Category';
 import { Course } from './Course';
 import { Episode } from './Episode';
 import { Favorite } from './Favorite';
 import { User } from './User';
+import { Like } from './Like';
 
 Category.hasMany(Course);
 
 Course.belongsTo(Category);
 Course.hasMany(Episode);
 Course.belongsToMany(User, { through: Favorite });
+Course.belongsToMany(User, { through: Like });
 Course.hasMany(Favorite, { as: 'favoritesUsers', foreignKey: 'course_id' });
 
 Episode.belongsTo(Course);
@@ -19,6 +19,7 @@ Favorite.belongsTo(Course);
 Favorite.belongsTo(User);
 
 User.belongsToMany(Course, { through: Favorite });
+User.belongsToMany(Course, { through: Like });
 User.hasMany(Favorite, { as: 'favoritesCourses', foreignKey: 'user_id' });
 
 export {
@@ -26,5 +27,6 @@ export {
   Course,
   Episode,
 	Favorite,
+  Like,
   User
 };
