81a36c21a072ebe6dc8cce5d39b45547fa502767
Gleriston <xpanium@gmail.com>
2023-07-12 12:34:33 -0300

    Atualizando os endpoints de cursos

--- src/services/favoriteService.ts
+++ src/services/favoriteService.ts
@@ -37,4 +37,15 @@ export const favoriteService = {
       }
     })
   },
+
+  isFavorited: async (userId: number, courseId: number) => {
+    const favorite = await Favorite.findOne({
+      where: {
+        userId,
+        courseId
+      }
+    })
+
+    return favorite !== null
+  }
 };
