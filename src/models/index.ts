9688c0e5964ad5e4800a2d565be45a9ce3132a38
Gleriston <xpanium@gmail.com>
2023-07-18 16:28:05 -0300

    fix error with front end

--- src/models/index.ts
+++ src/models/index.ts
@@ -6,31 +6,31 @@ import { User } from './User';
 import { Like } from './Like';
 import { WatchTime } from './WatchTime';
 
-Category.hasMany(Course);
+Category.hasMany(Course, { as: 'courses' })
 
-Course.belongsTo(Category);
-Course.hasMany(Episode);
-Course.belongsToMany(User, { through: Favorite });
-Course.belongsToMany(User, { through: Like });
-Course.hasMany(Favorite, { as: 'favoritesUsers', foreignKey: 'course_id' });
+Course.belongsTo(Category)
+Course.belongsToMany(User, { through: Favorite })
+Course.belongsToMany(User, { through: Like })
+Course.hasMany(Episode, { as: 'episodes' })
+Course.hasMany(Favorite, { as: 'FavoritesUsers', foreignKey: 'course_id' })
 
-Episode.belongsTo(Course);
-Episode.belongsToMany(User, { through: WatchTime});
+Episode.belongsTo(Course)
+Episode.belongsToMany(User, { through: WatchTime })
 
-Favorite.belongsTo(Course);
-Favorite.belongsTo(User);
+Favorite.belongsTo(Course)
+Favorite.belongsTo(User)
 
-User.belongsToMany(Course, { through: Favorite });
-User.belongsToMany(Course, { through: Like });
-User.belongsToMany(Episode, { through: WatchTime});
-User.hasMany(Favorite, { as: 'favoritesCourses', foreignKey: 'user_id' });
+User.belongsToMany(Course, { through: Favorite })
+User.belongsToMany(Course, { through: Like })
+User.belongsToMany(Episode, { through: WatchTime })
+User.hasMany(Favorite, { as: 'FavoritesCourses', foreignKey: 'user_id' })
 
 export {
-  Category,
-  Course,
-  Episode,
-	Favorite,
-  Like,
-  User,
-  WatchTime
-};
+    Category,
+    Course,
+    Episode,
+    Favorite,
+    Like,
+    User,
+    WatchTime
+}
